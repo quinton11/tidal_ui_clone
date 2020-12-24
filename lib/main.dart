3ffe375f250d81dc4601741b9d11278d9d4120c2
quinton11 <quintons831@gmail.com>
2020-12-24 22:37:53 +0000

    Completed bottom navigation bar look

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import "custom_icons/elusive_video_icons.dart";
+//import "custom_icons/tidal_icon_icons.dart";
+import "custom_icons/home_icon_icons.dart";
 
 void main() {
   runApp(MyApp());
@@ -20,49 +23,27 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        //primarySwatch: Colors.black,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: "KumbhSans",
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  //Holds current Index of selected icon in bottomdrawer
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -75,43 +56,62 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Tidal UI"),
+        backgroundColor: Color.fromRGBO(0, 0, 0, 0)
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Text(
+          "HomePage",
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10.0,
+        backgroundColor: Colors.black,
+        currentIndex: _currentIndex,
+        unselectedItemColor: Color.fromRGBO(92, 88, 88, 1),
+        onTap: (int i) {
+          setState(() {
+            _currentIndex = i;
+          });
+        },
+        selectedItemColor: Color.fromRGBO(3, 252, 186, 1),
+
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              HomeIcon.home,
+            ),
+            label: "Home",
+            backgroundColor: Color.fromRGBO(44, 51, 49, 0),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              ElusiveVideo.video,
+            ),
+            label: "Videos",
+            backgroundColor: Color.fromRGBO(44, 51, 49, 0),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+            ),
+            label: "Explore",
+            backgroundColor: Color.fromRGBO(44, 51, 49, 0),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline),
+            label: "My Collection",
+            backgroundColor: Color.fromRGBO(44, 51, 49, 0),
+          )
+        ],
+      ),
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
