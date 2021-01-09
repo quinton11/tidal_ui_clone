9d2ab866e38f18fb7555b17a771804ecfe5c4391
quinton11 <quintons831@gmail.com>
2021-01-09 21:53:14 +0000

    completed album grid views

--- lib/widgets/scroll_section.dart
+++ lib/widgets/scroll_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tidal_ui/screens/explore_screen.dart';
 //import 'package:provider/provider.dart';
 
 import 'package:tidal_ui/widgets/album_view.dart';
@@ -38,7 +39,15 @@ class ScrollSection extends StatelessWidget {
                   ),
                 ),
                 TextButton(
-                  onPressed: null,
+                  onPressed: () => Navigator.push(
+                    context,
+                    FadeRoute(
+                      page: AlbumGridScreen(
+                        albums: albums,
+                        title: title,
+                      ),
+                    ),
+                  ),
                   child: Text(
                     "View all",
                     style: TextStyle(
@@ -76,34 +85,59 @@ class ScrollSection extends StatelessWidget {
   }
 }
 
-/* 
-SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Container(
-                      width: 145,
-                      height: 150,
-                      color: Colors.amber,
-                    ),
-                    SizedBox(
-                      width: 16,
-                    ),
-                    Container(
-                      width: 145,
-                      height: 150,
-                      color: Colors.amber[200],
-                    ),
-                    SizedBox(
-                      width: 16,
-                    ),
-                    Container(
-                      width: 145,
-                      height: 150,
-                      color: Colors.amber[600],
-                    ),
-                  ],
-                ),
-              ),
-*/
+class AlbumGridScreen extends StatelessWidget {
+  final title;
+  final albums;
+
+  AlbumGridScreen({
+    this.title,
+    this.albums,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        color: Colors.black,
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            childAspectRatio: 0.8,
+            crossAxisSpacing: 5,
+            mainAxisSpacing: 5,
+            maxCrossAxisExtent: 170,
+
+          ),
+          scrollDirection: Axis.vertical,
+          itemBuilder: (ctx, index) => AlbumView(
+            albumname: albums[index].title,
+            artists: albums[index].artistes,
+            explicit: albums[index].explicit,
+            image: albums[index].image,
+            mastered: albums[index].mastered,
+          ),
+          itemCount: albums.length,
+          physics: BouncingScrollPhysics(),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/scrollsection_track.dart
+++ lib/widgets/scrollsection_track.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tidal_ui/screens/explore_screen.dart';
 
 import 'track_view.dart';
 
@@ -6,7 +7,10 @@ class TrackScrollSection extends StatelessWidget {
   final String title;
   final tracks;
 
-  TrackScrollSection({this.title, this.tracks});
+  TrackScrollSection({
+    this.title,
+    this.tracks,
+  });
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -33,7 +37,17 @@ class TrackScrollSection extends StatelessWidget {
                   ),
                 ),
                 TextButton(
-                  onPressed: null,
+                  autofocus: false,
+                  clipBehavior: Clip.none,
+                  onPressed: () => Navigator.push(
+                    context,
+                    FadeRoute(
+                      page: TrackListScreen(
+                        title: title,
+                        tracks: tracks,
+                      ),
+                    ),
+                  ),
                   child: Text(
                     "View all",
                     style: TextStyle(
@@ -66,6 +80,56 @@ class TrackScrollSection extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class TrackListScreen extends StatelessWidget {
+  final title;
+  final tracks;
+
+  TrackListScreen({
+    this.title,
+    this.tracks,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        color: Colors.black,
+        child: ListView.separated(
+            itemBuilder: (ctx, index) => TrackView(
+                  artists: tracks[index].artists,
+                  explicit: true,
+                  image: tracks[index].image,
+                  mastered: true,
+                  trackname: tracks[index].title,
+                ),
+            separatorBuilder: (ctx, index) => SizedBox(
+                  height: 15,
+                ),
+            itemCount: tracks.length),
       ),
     );
   }
